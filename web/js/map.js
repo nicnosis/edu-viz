921fa3e4ef372f246731137dd84a33b7c60b4e98
mtgNic <nic.jsb@gmail.com>
2015-10-08 12:27:47 -0400

    refactor for page load speed

---
+++ web/js/map.js
@@ -0,0 +1,21 @@
+var map = L.map('map').setView([38.61687, -98.58032], 7);
+var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+    attribution: 'Tiles &copy; Esri;'
+}).addTo(map);
+
+// Initial viz
+$(document).ready(function() {
+
+    d3.csv ("MERGED2013_PP.csv", function(error, data) {
+        data.forEach(function(d) {
+            d.LATITUDE = +d.LATITUDE;
+            d.LONGITUDE = +d.LONGITUDE;
+            console.log (d.LATITUDE + ", " + d.LONGITUDE);
+        });
+    });
+
+})
+
+var coords = []; // global
+
