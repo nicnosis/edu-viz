afd97113fc54039c3d909862556cf414db17cb58
mtgNic <nic.jsb@gmail.com>
2016-04-16 13:27:16 -0400

    add agile approach pdf

--- data/1_raw-data/sedscript.bash
+++ data/1_raw-data/sedscript.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 unset LANG
-for filename in MERG*.csv; do
+
+cd 1_raw-data
+for filename in merg*.csv; do
 	echo $filename
 	sed -e "s/NULL//g" $filename > nonulls.csv
 	sed -e "s/PrivacySuppressed//g" nonulls.csv >  clean_$filename
+
+	mv clean_$filename ../2_cleaned-data
 done
