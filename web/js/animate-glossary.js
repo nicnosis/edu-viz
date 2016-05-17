d498bee664701b8b3bee08d5e47d2dacaeeddf8b
mtgNic <nic.jsb@gmail.com>
2016-05-17 17:08:07 -0400

    working glossary sidebar nav

--- web/js/animate-glossary.js
+++ web/js/animate-glossary.js
@@ -18,29 +18,41 @@ if (!("ontouchstart" in window)) {
     }
 
     $glossary.on("click", "a", function(event) {
-        var name = $(this).attr("href");
+        // Get the anchor's href
+        var href = $(this).attr("href");
+        console.log("name - " + href);
         event.preventDefault();
 
-        $(name).velocity("scroll", { offset: -(8), complete: function() {
+        $(href).velocity("scroll", { offset: -(8), complete: function() {
             $(this).attr("id", "");
-            window.location.hash = name.substr(1);
-            $(this).attr("id", name.substr(1));
+            window.location.hash = href.substr(0); // should these be 0 or 1?
+            $(this).attr("id", href.substr(0));
+
+            console.log("velocity scroll to " + href + " complete.");
         }});
     });
 
     var section,
         sections = {},
-        $contentPanes = $("#documentation > li[id]"), // #documentation is a <ul>
+        $contentPanes = $("#main-content > li[id]"), // #documentation is a <ul>
         $oldBest;
 
     $contentPanes.each(function(){
         var $this = $(this),
             // words = $this.find(".dataHeaderTitle").text().split(":");
-            anchor = $this.find("h1").text();
+            $h1Text = $this.find("h1").text();
+        
+        console.log("sections");
+        console.log(sections);
 
 
         // sections[words[0]] = sections[words[0]] || [];
-        sections[anchor].push([ $this.attr("id"), anchor ]);
+        // sections[words[0]].push([ $this.attr("id"), words[1] ]);
+        sections[$h1Text] = sections[$h1Text] || [];
+        sections[$h1Text].push([ $this.attr("id"), $h1Text ]);
+
+        console.log("sections");
+        console.log(sections);
 
     });
 
@@ -51,9 +63,13 @@ if (!("ontouchstart" in window)) {
         for (var j = 0; j < section.length; j++) {
             $section.append("<li><a href=\"#" + section[j][0] + "\">" + section[j][0] + "</a></li>");
         }
+
+        console.log(section);
     }
 
     $(window).on("scroll", function() {
+        console.log("window on scroll invoked.");
+        
         $contentPanes.each(function(){
             var $this = $(this),
                 offset = $this.offset();
